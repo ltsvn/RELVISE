8306ec277f2a155c3c590541cbf5a70906c4e47a
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-03-12 15:52:05 +0100

    connect popup to button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
 function emailValidation() {
@@ -33,8 +33,13 @@ function emailSubmitButton(){
     let popUp = document.querySelector('.popup');
     let popupForm = document.querySelector('.form');
     let openPopupButton = document.querySelector('.main-block_button_orange');
+    let closePopupButton = document.querySelector('.close')
 
-    openPopupButton.addEventListener('click', function() {
-        function.preventDefault();
+    closePopupButton.addEventListener('click', function() {
+        popUp.classList.remove('active');
+    })
+
+    openPopupButton.addEventListener('click', function(e) {
+        e.preventDefault();
         popUp.classList.add('active');
     })
